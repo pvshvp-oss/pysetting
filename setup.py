f274253359865b521c91a76f3c638c9830b82848
shivanandvp <shivanand.pattanshetti@gmail.com>
2021-02-15 11:11:32 -0600

    .

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="fenix_library-analyzing",
-    version="0.0.1",
+    version="0.0.2",
     author="Shivanand Pattanshetti",
     author_email="shivanand.pattanshetti@gmail.com",
     description="The analysis library for the Fenix Installer",
