aaffe8b0b2344433ad9673ed34f54676824f1d4d
shivanandvp <shivanand.pattanshetti@gmail.com>
2021-03-29 03:13:48 -0500

    .

--- scripts/regenerate_pipenv.sh
+++ scripts/regenerate_pipenv.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env sh
 
 # Clean pipenv
-pipenv uninstall fenix-library-running fenix-library-configuration sphinx sphinx-rtd-theme sphinxcontrib-mermaid setuptools build
-sudo pipenv uninstall fenix-library-running fenix-library-configuration sphinx sphinx-rtd-theme sphinxcontrib-mermaid setuptools build
+pipenv uninstall fenix-library-running fenix-library-configuration setuptools build sphinx sphinx-rtd-theme sphinxcontrib-mermaid
+sudo pipenv uninstall fenix-library-running fenix-library-configuration setuptools build sphinx sphinx-rtd-theme sphinxcontrib-mermaid
 pipenv clean
 sudo pipenv clean
 pipenv --rm
@@ -13,7 +13,7 @@ pipenv graph
 sudo pipenv graph
 
 # Regenerate pipenv
-sudo pipenv install --dev -e git+https://gitlab.com/rebornos-team/fenix/libraries/running.git#egg=fenix_library-running -e git+https://gitlab.com/rebornos-team/fenix/libraries/configuration.git#egg=fenix_library-configuration sphinx sphinx-rtd-theme sphinxcontrib-mermaid setuptools build
+sudo pipenv install --dev -e git+https://gitlab.com/rebornos-team/fenix/libraries/running.git#egg=fenix_library-running -e git+https://gitlab.com/rebornos-team/fenix/libraries/configuration.git#egg=fenix_library-configuration setuptools build sphinx sphinx-rtd-theme sphinxcontrib-mermaid
 
 # Check if pipenv is regenerated
 pipenv graph
