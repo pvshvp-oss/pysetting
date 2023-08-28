2b3a630adf185cc306e6354d17e10d34a079401d
shivanandvp <shivanandvp.oss@gmail.com>
2023-08-28 07:36:19 -0500

    build: :bookmark: Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pysetting",
-    version="0.0.5",
+    version="0.0.6",
     author="shivanandvp",
     author_email="shivanandvp.oss@gmail.com",
     description="A python library for parsing and storing settings and configurations.",
