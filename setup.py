b5872cd70845aeec28d6a39290e9be24944ad597
pvshvp-oss <pvshvp.oss@gmail.com>
2024-04-29 21:37:55 -0500

    Bump versions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pysetting",
-    version="0.0.6",
+    version="0.0.7",
     author="shiva.patt",
     author_email="shiva.patt.oss@gmail.com",
     description="A python library for parsing and storing settings and configurations.",
